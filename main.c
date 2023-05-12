1dba9402d55a83fe6224c5bf74de86a130cacaf5
leonlux <leonlux@icloud.com>
2023-05-12 12:28:00 +0200

    obtain path and current working dir

--- main.c
+++ main.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/syslimits.h>
+
+/*
+ * Queries the path variable and returns a copy of it
+ * */
+char *get_path() {
+  char *envvar = "PATH";
+  int path_len = strlen(getenv(envvar));
+
+  char *path = (char*) malloc(path_len);
+  strncpy(path, getenv(envvar), path_len);
+  return path;
+}
+
+/*
+ * Queries the current working directory
+ * */
+char *get_cwd() {
+  char *cwd = (char*) malloc(PATH_MAX);
+  getcwd(cwd, PATH_MAX);
+  return cwd;
+}
 
 int main(int argc, char **argv) {
-  printf("Hello, world!\n");
+  const char* path = get_path();
+  const char* cwd = get_cwd();
+
+  if(path == NULL) {
+    printf("path is null\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(cwd == NULL) {
+    printf("current working directory is null\n");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Path: %s\n", path);
+  printf("CWD: %s\n", cwd);
+
+  free((void*) path);
+  free((void*) cwd);
   exit(EXIT_SUCCESS);
 }
