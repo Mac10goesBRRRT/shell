d1192da19f83a14044d9703009136c9d4b1a5584
leonlux <leonlux@icloud.com>
2023-05-13 13:00:57 +0200

    implement cd command

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/syslimits.h>
+#include <sys/stat.h>
 
 /*
  * Queries the path variable and returns a copy of it
@@ -26,8 +27,10 @@ char *get_cwd() {
 }
 
 int main(int argc, char **argv) {
-  const char* path = get_path();
-  const char* cwd = get_cwd();
+  const char *path = get_path();
+  char *cwd = get_cwd();
+  const char *prompt = "$> ";
+
 
   if(path == NULL) {
     printf("path is null\n");
@@ -39,8 +42,44 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
-  printf("Path: %s\n", path);
-  printf("CWD: %s\n", cwd);
+  for(;;) {
+
+    printf("%s %s", cwd, prompt);
+
+    size_t len = 0;
+    char *cmd = (char*) malloc(1);
+    char c;
+    while((c = getchar()) != '\n' && c != EOF) {
+      char *t = realloc(cmd, len + 1);
+      cmd = t;
+      cmd[len] = c;
+      len += 1;
+    }
+
+    char *token = strtok(cmd, " ");
+
+    if(strcmp(token, "pwd") == 0) {
+      printf("%s\n", cwd);
+    } else if(strcmp(token, "exit") == 0) {
+      free(cmd);
+      break;
+    } else if(strcmp(token, "cd") == 0) {
+      char *folder = strtok(NULL, "");
+      
+      if(chdir(folder) != 0) {
+        printf("cd: path %s does not exist\n", folder);
+      } else {
+        free(cwd);
+        cwd = get_cwd();
+      }
+      free(cmd);
+    } else {
+      free(cmd);
+      continue;
+    }
+  }
+
+  printf("Bye\n");
 
   free((void*) path);
   free((void*) cwd);
