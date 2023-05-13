220f714241df3ab6c72e7733d0cc98b1f4c40c71
leonlux <leonlux@icloud.com>
2023-05-13 14:57:15 +0200

    added support for running commands from PATH and refactored

--- main.c
+++ main.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <dirent.h>
 #include <sys/syslimits.h>
 #include <sys/stat.h>
-
-/*
- * Queries the path variable and returns a copy of it
- * */
-char *get_path() {
-  char *envvar = "PATH";
-  int path_len = strlen(getenv(envvar));
-
-  char *path = (char*) malloc(path_len);
-  strncpy(path, getenv(envvar), path_len);
-  return path;
-}
-
+#include <sys/types.h>
 /*
  * Queries the current working directory
  * */
@@ -26,16 +17,60 @@ char *get_cwd() {
   return cwd;
 }
 
-int main(int argc, char **argv) {
-  const char *path = get_path();
-  char *cwd = get_cwd();
-  const char *prompt = "$> ";
+void launch_cmd(char **args) {
+  pid_t pid;
+  int status;
 
+  pid = fork();
+  if(pid == 0) {
+    if(execvp(args[0], args) == -1) {
+      printf("error launching command process\n");
+    }
+    exit(EXIT_FAILURE);
+  } else if(pid < 0) {
+    printf("error forking\n");
+  } else {
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+}
 
-  if(path == NULL) {
-    printf("path is null\n");
+char **tokenize_cmd(char* cmd) {
+  int buffer_size = 64;
+  int pos = 0;
+  char **tokens = malloc(buffer_size * sizeof(char*));
+  char *token, **tokens_backup;
+
+  if(!tokens) {
+    printf("error allocating\n");
     exit(EXIT_FAILURE);
   }
+
+  token = strtok(cmd, " ");
+  while(token != NULL) {
+    tokens[pos] = token;
+    pos += 1;
+
+    if(pos >= buffer_size) {
+      buffer_size += buffer_size;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, buffer_size * sizeof(char*));
+      if(!tokens) {
+        free(tokens_backup);
+        printf("error growing the command array");
+        exit(EXIT_FAILURE);
+      }
+    }
+    token = strtok(NULL, " ");
+  }
+  tokens[pos] = NULL;
+  return tokens;
+}
+
+int main(int argc, char **argv) {
+  char *cwd = get_cwd();
+  const char *prompt = "$> ";
 
   if(cwd == NULL) {
     printf("current working directory is null\n");
@@ -56,15 +91,21 @@ int main(int argc, char **argv) {
       len += 1;
     }
 
-    char *token = strtok(cmd, " ");
+    if(len == 0) {
+      free(cmd);
+      continue;
+    }
 
-    if(strcmp(token, "pwd") == 0) {
+    char **tokens = tokenize_cmd(cmd);
+
+    if(strcmp(tokens[0], "pwd") == 0) {
       printf("%s\n", cwd);
-    } else if(strcmp(token, "exit") == 0) {
+      free(cmd);
+    } else if(strcmp(tokens[0], "exit") == 0) {
       free(cmd);
       break;
-    } else if(strcmp(token, "cd") == 0) {
-      char *folder = strtok(NULL, "");
+    } else if(strcmp(tokens[0], "cd") == 0) {
+      char *folder = tokens[1];
       
       if(chdir(folder) != 0) {
         printf("cd: path %s does not exist\n", folder);
@@ -74,14 +115,12 @@ int main(int argc, char **argv) {
       }
       free(cmd);
     } else {
-      free(cmd);
-      continue;
+      launch_cmd(tokens);
     }
   }
 
   printf("Bye\n");
 
-  free((void*) path);
   free((void*) cwd);
   exit(EXIT_SUCCESS);
 }
